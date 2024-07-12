d3163b4730810ee361848ddcef22bb1fc8575399
babyo77 <onlybabyyo7@gmail.com>
2024-07-12 23:33:57 +0530

    🚀

--- src/renderer/src/components/header.tsx
+++ src/renderer/src/components/header.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable prettier/prettier */
 
 import { GoDeviceDesktop } from 'react-icons/go'
+import { IoArrowDownOutline } from 'react-icons/io5'
 import { VscDebugRestart } from 'react-icons/vsc'
-function Header({ connected }: { connected: boolean; length: number }): JSX.Element {
+function Header({ connected, length }: { connected: boolean; length: number }): JSX.Element {
   return (
     <header
       className={`border-b p-4 flex justify-between ${connected ? '' : 'text-zinc-400'} items-center`}
@@ -14,6 +15,9 @@ function Header({ connected }: { connected: boolean; length: number }): JSX.Elem
         </p>
       </div>
       <div className="flex gap-3">
+        <p className="flex items-center gap-1">
+          <IoArrowDownOutline /> {length}
+        </p>
         <button
           onClick={() => window.location.reload()}
           className="text-zinc-100 flex items-center gap-1"
